c74ed1fdd818323aa80d160f5e17bf04315766da
dhannypramana <p.dhannypramana@gmail.com>
2022-02-16 23:46:00 +0700

    Pertemuan 14 : Namespace

---
+++ pertemuan14/App/Produk/User.php
@@ -0,0 +1,10 @@
+<?php namespace App\Produk;
+
+    class User
+    {
+        public function __construct() 
+        {
+            echo "Ini adalah class " . __CLASS__ . "<br>";
+        }
+    }
+    
--- pertemuan14/index.php
+++ pertemuan14/index.php
@@ -1,16 +1,21 @@
 <?php
+    require_once 'App/init.php';
+
     // Pertemuan 14 : Namespace
     // sebuah cara untuk mengelompokkan program ke dalam sebuah package tersendiri
+    // ada juga yang menyebutnya teknik Encapsulation
 
-    spl_autoload_register(function ($class)
-    {
-        require_once __DIR__.'/App/Produk/'.$class.'.php';
-    });
-
-    $produk1 = new Anime("Attack On Titan", "MAPPA", "Action", 30000, 80);
-    $produk2 = new Game("Grand Theft Auto", "Rockstar Studio", "Gangster Life", 50000, 50);
+    // $produk1 = new Anime("Attack On Titan", "MAPPA", "Action", 30000, 80);
+    // $produk2 = new Game("Grand Theft Auto", "Rockstar Studio", "Gangster Life", 50000, 50);
     
-    $cetakProduk = new CetakInfoProduk();
-    $cetakProduk->addProduk($produk1);
-    $cetakProduk->addProduk($produk2);
-    echo $cetakProduk->cetak();
+    // $cetakProduk = new CetakInfoProduk();
+    // $cetakProduk->addProduk($produk1);
+    // $cetakProduk->addProduk($produk2);
+    // echo $cetakProduk->cetak();
+
+    // Alias
+    use App\Produk\User as ProdukUSer;
+    use App\Service\User as ServiceUser;
+
+    new ProdukUSer();
+    new ServiceUser();
